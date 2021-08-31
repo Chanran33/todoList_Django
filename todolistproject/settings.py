e078a7fb8839d30197c2618d2728e8c8febce65d
Tae Young <k55416325@gamil.com>
2021-08-31 14:05:01 +0900

    os

--- todolistproject/settings.py
+++ todolistproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-!ursoqb=xq4l80x5-jbk(bcf$ft@2_rtam2_noe-&f0qtvqq92')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
